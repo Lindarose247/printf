aefe89b1d032f70d3c37c7738438a5db09e45a85
sbonele <sbonelemiya@gmail.com>
2022-11-19 21:14:44 +0200

    task0

---
+++ _printf.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _printf - works like prinf
+ * @format: the format string
+ * Return: number of bytes printed or -1 on error
+ */
+
+int _printf(const char *format, ...)
+
+{
+
+        int i, total = 0;
+
+        char c;
+
+        va_list args;
+
+        int (*print)(va_list *);
+
+        va_start(args, format);
+
+        if (!format)
+
+                return (0);
+
+        for (i = 0; format[i] != '\0'; i++)
+
+        {
+
+                if (format[i] == '%')
+
+                {
+
+                        i++;
+
+                        c = format[i];
+
+                        if (c == '%')
+
+                        {
+
+                                total += write(1, "%", 1);
+
+                                continue;
+
+                        }
+
+                        print = get_f(c);
+
+                        total += print(&args);
+
+                }
+
+                else
+
+                        total += write(1, &format[i], 1);
+
+        }
+
+        va_end(args);
+
+        return (total);
+
+}
---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <unistd.h>
+
+#include <stdarg.h>
+
+#include <stdlib.h>
+
+/**
+ * struct sp - special characters
+ * @sc: special character
+ * @f: function
+ */
+
+typedef struct sp
+
+{
+
+        char sc;
+
+        int (*f)(va_list *);
+
+} sp;
+
+int _printf(const char *format, ...);
+
+int print_c(va_list *);
+
+int print_s(va_list *);
+
+int (*get_f(char))(va_list *);
+
+
+
+#endif
