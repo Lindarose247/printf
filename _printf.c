4f10c7376a1be6842a5414009d425fd6870d5a07
sbonele <sbonelemiya@gmail.com>
2022-11-19 22:03:27 +0200

    task0

--- _printf.c
+++ _printf.c
@@ -1,65 +1,82 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _printf - works like prinf
- * @format: the format string
- * Return: number of bytes printed or -1 on error
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
  */
-
-int _printf(const char *format, ...)
-
+static int (*check_for_specifiers(const char *format))(va_list)
 {
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
-        int i, total = 0;
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
 
-        char c;
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-        va_list args;
-
-        int (*print)(va_list *);
-
-        va_start(args, format);
-
-        if (!format)
-
-                return (0);
-
-        for (i = 0; format[i] != '\0'; i++)
-
-        {
-
-                if (format[i] == '%')
-
-                {
-
-                        i++;
-
-                        c = format[i];
-
-                        if (c == '%')
-
-                        {
-
-                                total += write(1, "%", 1);
-
-                                continue;
-
-                        }
-
-                        print = get_f(c);
-
-                        total += print(&args);
-
-                }
-
-                else
-
-                        total += write(1, &format[i], 1);
-
-        }
-
-        va_end(args);
-
-        return (total);
-
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,37 +1,32 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-#include <unistd.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
-
-#include <stdlib.h>
-
 /**
- * struct sp - special characters
- * @sc: special character
- * @f: function
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-
-typedef struct sp
-
+typedef struct print
 {
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-        char sc;
-
-        int (*f)(va_list *);
-
-} sp;
-
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-int print_c(va_list *);
-
-int print_s(va_list *);
-
-int (*get_f(char))(va_list *);
-
-
-
-#endif
+#endif  /* _MAIN_H */
